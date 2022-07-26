a7b8117ac8655d91e99f5a999441f91a9568ef19
Fernanda Romao <Fernanda Romao>
2022-07-25 22:00:26 -0300

    requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,13 @@
 SELECT 
-    c.nome AS nome,
-    COUNT(DISTINCT (h.usuario_id)) AS reproducoes
+    c.nome AS nome, COUNT(h.cancao_id) AS reproducoes
 FROM
     SpotifyClone.cancoes AS c
         INNER JOIN
     SpotifyClone.historico_reproducao AS h ON c.cancao_id = h.cancao_id
+        INNER JOIN
+    SpotifyClone.usuarios AS u ON h.usuario_id = u.usuario_id
+WHERE
+    (u.plano_id = 1 OR u.plano_id = 4)
 GROUP BY c.nome
 ORDER BY c.nome;
+
